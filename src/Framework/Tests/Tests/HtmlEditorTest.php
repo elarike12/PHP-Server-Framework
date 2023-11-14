2483ab04c1f44c6b67d45f64b6b2cad8a1654a20
elarike12 <elarike12@gmail.com>
2023-11-14 23:34:37 +0200

    Updated html editor tests

--- src/Framework/Tests/Tests/HtmlEditorTest.php
+++ src/Framework/Tests/Tests/HtmlEditorTest.php
@@ -17,21 +17,72 @@ class HtmlEditorTest extends TestCase {
         <title>Framework</title>
     </head>
     <body>
+        <div>
+            <div>Test 1</div>
+            <div>Test 2</div>
+            <div>Test 3</div>
+            Test 4
+        </div>
     </body>
 </html>';
         $this->htmlEditor = new HtmlEditor($html);
     }
 
+    public function testSearch() {
+        $search = $this->htmlEditor->search('//body/div/div[1]')[0] ?? null;
+        $this->assertInstanceOf(HtmlEditor::class, $search);
+        $this->assertSame('<div>Test 1</div>', trim($search->getHtmlContent()));
+        $search = $this->htmlEditor->search('//body/div/div[2]')[0] ?? null;
+        $this->assertInstanceOf(HtmlEditor::class, $search);
+        $this->assertSame('<div>Test 2</div>', trim($search->getHtmlContent()));
+        $search = $this->htmlEditor->search('//body/div/text()[4]')[0] ?? null;
+        $this->assertInstanceOf(HtmlEditor::class, $search);
+        $this->assertSame('Test 4', trim($search->getHtmlContent()));
+    }
+
+    public function testSearchNoResults() {
+        $search = $this->htmlEditor->search('//body/p');
+        $this->assertEmpty($search);
+    }
+
+    public function testGetChildren() {
+        $search = $this->htmlEditor->search('//body/div')[0]->getChildren();
+        $this->assertCount(4, $search);
+    }
+
+    public function testGetNthChildren() {
+        $search = $this->htmlEditor->search('//body/div')[0];
+        $this->assertSame('<div>Test 1</div>', trim($search->getChildren(nth: 1)[0]->getHtmlContent()));
+        $this->assertSame('<div>Test 2</div>', trim($search->getChildren(nth: 2)[0]->getHtmlContent()));
+        $this->assertSame('<div>Test 3</div>', trim($search->getChildren(nth: 3)[0]->getHtmlContent()));
+        $this->assertSame('Test 4', trim($search->getChildren(nth: 4)[0]->getHtmlContent()));
+    }
+
+    public function testGetParent() {
+        $search = $this->htmlEditor->search('//body/div/div[1]')[0];
+        $search = $search->getParent()->getChildren();
+        $this->assertCount(4, $search);
+    }
+
+    public function testSearchCloneResults() {
+        $search = $this->htmlEditor->search('//body/div/div[1]', true)[0];
+        $this->assertNull($search->getParent());
+    }
+
     public function testAppend() {
-        $this->htmlEditor->append('<div>Test 1</div>', '//body', true);
-        $this->assertSame('<div>Test 1</div>', trim($this->htmlEditor->getChildren('//body')[0]->getHtmlContent()));
-        $this->htmlEditor->append('<div>Test 2</div>', '//body', true);
-        $this->assertSame('<div>Test 2</div>', trim($this->htmlEditor->getChildren('//body')[1]->getHtmlContent()));
-        $this->htmlEditor->append('<div>Test 3</div>', '//body', false);
-        $this->assertSame('<div>Test 3</div>', trim($this->htmlEditor->getNextSiblings('//body')[0]->getHtmlContent()));
-        $this->htmlEditor->append('<div>Test 4</div>', '//body', false);
-        $this->assertSame('<div>Test 4</div>', trim($this->htmlEditor->getNextSiblings('//body')[0]->getHtmlContent()));
-        $this->assertSame('<div>Test 3</div>', trim($this->htmlEditor->getNextSiblings('//body')[1]->getHtmlContent()));
+        $this->htmlEditor->append('<div>Test 5</div>', '//body/div[1]');
+        $this->assertSame('<div>Test 5</div>', trim($this->htmlEditor->getChildren('//body')[1]->getHtmlContent()));
+        $this->htmlEditor->append('<div>Test 6</div>', '//body/div[1]');
+        $this->assertSame('<div>Test 6</div>', trim($this->htmlEditor->getChildren('//body')[1]->getHtmlContent()));
+        $this->assertSame('<div>Test 5</div>', trim($this->htmlEditor->getChildren('//body')[2]->getHtmlContent()));
+    }
+
+    public function testInnerHtml() {
+        $this->htmlEditor->append('<div>Test 5</div>', '//body/div', true);
+        $this->assertSame('<div>Test 5</div>', trim($this->htmlEditor->getChildren('//body/div')[4]->getHtmlContent()));
+        $this->htmlEditor->append('<div>Test 6</div>', '//body/div', true);
+        $this->assertSame('<div>Test 6</div>', trim($this->htmlEditor->getChildren('//body/div')[5]->getHtmlContent()));
+        $this->assertSame('<div>Test 5</div>', trim($this->htmlEditor->getChildren('//body/div')[4]->getHtmlContent()));
     }
 
     public function testPrepend() {
@@ -48,41 +99,36 @@ class HtmlEditorTest extends TestCase {
     }
 
     public function testReplace() {
-        $this->htmlEditor->append('<div>Test 1</div>', '//body', true);
-        $this->htmlEditor->append('<div>Test 2</div>', '//body', true);
-        $this->htmlEditor->getChildren('//body')[0]->replace('<div>Test 3</div>');
-        $this->assertSame('<div>Test 3</div>', trim($this->htmlEditor->getChildren('//body')[0]->getHtmlContent()));
-        $this->assertSame('<div>Test 2</div>', trim($this->htmlEditor->getChildren('//body')[1]->getHtmlContent()));
-        $this->htmlEditor->getChildren('//body')[0]->replace('<p>Test 3</p>', innerHtml: true);
-        $this->assertSame('<div><p>Test 3</p></div>', trim($this->htmlEditor->getChildren('//body')[0]->getHtmlContent()));
+        $this->htmlEditor->getChildren('//body/div')[0]->replace('<p>Test 1</p>');
+        $this->assertSame('<p>Test 1</p>', trim($this->htmlEditor->getChildren('//body/div')[0]->getHtmlContent()));
+        $this->htmlEditor->getChildren('//body/div')[1]->replace('<p>Test 2</p>');
+        $this->assertSame('<p>Test 2</p>', trim($this->htmlEditor->getChildren('//body/div')[1]->getHtmlContent()));
+    }
+
+    public function testReplaceInnerHtml() {
+        $this->htmlEditor->getChildren('//body/div')[0]->replace('<p>Test 1</p>', innerHtml: true);
+        $this->assertSame('<div><p>Test 1</p></div>', trim($this->htmlEditor->getChildren('//body/div')[0]->getHtmlContent()));
+        $this->htmlEditor->getChildren('//body/div')[1]->replace('<p>Test 2</p>', innerHtml: true);
+        $this->assertSame('<div><p>Test 2</p></div>', trim($this->htmlEditor->getChildren('//body/div')[1]->getHtmlContent()));
     }
 
     public function testRemove() {
-        $this->htmlEditor->append('<div>Test 1</div><div>Test 2</div><div>Test 3</div>', '//body', true);
-        $this->htmlEditor->getChildren('//body')[1]->remove();
-        $this->assertEquals(2, count($this->htmlEditor->getChildren('//body')));
-        $this->assertSame('<div>Test 1</div>', trim($this->htmlEditor->getChildren('//body')[0]->getHtmlContent()));
-        $this->assertSame('<div>Test 3</div>', trim($this->htmlEditor->getChildren('//body')[1]->getHtmlContent()));
-
-        $this->htmlEditor->getChildren('//body')[1]->remove(innerHtml: true);
-        $this->assertSame('<div></div>', trim($this->htmlEditor->getChildren('//body')[1]->getHtmlContent()));
+        $this->htmlEditor->getChildren('//body/div')[0]->remove();
+        $this->assertSame('<div>Test 2</div>', trim($this->htmlEditor->getChildren('//body/div')[0]->getHtmlContent()));
+        $this->htmlEditor->getChildren('//body/div')[0]->remove();
+        $this->assertSame('<div>Test 3</div>', trim($this->htmlEditor->getChildren('//body/div')[0]->getHtmlContent()));
+        $this->htmlEditor->getChildren('//body/div')[0]->remove();
+        $this->assertSame('Test 4', trim($this->htmlEditor->getChildren('//body/div')[0]->getHtmlContent()));
 
     }
 
     public function testAttributes() {
-        $this->htmlEditor->append('<div><div></div></div>', '//body', true);
-        $this->htmlEditor->getChildren('//body')[0]->addAttributes(['test1' => 'test 1']);
-        $this->htmlEditor->getChildren('//body')[0]->addAttributes(['test2' => 'test 2']);
-        $this->htmlEditor->getChildren('//body')[0]->getChildren()[0]->addAttributes(['test3' => 'test 3']);
-        $this->htmlEditor->getChildren('//body')[0]->getChildren()[0]->addAttributes(['test4' => 'test 4']);
-        $this->assertSame('<div test1="test 1" test2="test 2"><div test3="test 3" test4="test 4"></div></div>', trim($this->htmlEditor->getChildren('//body')[0]->getHtmlContent()));
+        $this->htmlEditor->getChildren('//body/div')[0]->addAttributes(['test1' => 'test 1']);
+        $this->htmlEditor->getChildren('//body/div')[1]->addAttributes(['test2' => 'test 2']);
+        $this->htmlEditor->getChildren('//body/div')[2]->addAttributes(['test3' => 'test 3']);
 
-        $this->htmlEditor->getChildren('//body')[0]->setAttributes(['test5' => 'test 5']);
-        $this->htmlEditor->getChildren('//body')[0]->getChildren()[0]->setAttributes(['test6' => 'test 6']);
-        $this->assertSame('<div test5="test 5"><div test6="test 6"></div></div>', trim($this->htmlEditor->getChildren('//body')[0]->getHtmlContent()));
-
-        $this->htmlEditor->getChildren('//body')[0]->getChildren()[0]->removeAttributes(['test6']);
-        $this->htmlEditor->getChildren('//body')[0]->removeAttributes(['test5']);
-        $this->assertSame('<div><div></div></div>', trim($this->htmlEditor->getChildren('//body')[0]->getHtmlContent()));
+        $this->assertSame('<div test1="test 1">Test 1</div>', trim($this->htmlEditor->getChildren('//body/div')[0]->getHtmlContent()));
+        $this->assertSame('<div test2="test 2">Test 2</div>', trim($this->htmlEditor->getChildren('//body/div')[1]->getHtmlContent()));
+        $this->assertSame('<div test3="test 3">Test 3</div>', trim($this->htmlEditor->getChildren('//body/div')[2]->getHtmlContent()));
     }
 }
